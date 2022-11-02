775b2b7bf245f0a0ced777d67fa3f3a5a8537f4d
Nicolas Moraes <62454946+moraes7@users.noreply.github.com>
2022-11-02 15:39:40 -0300

    aula03

---
+++ main.js
@@ -0,0 +1,34 @@
+function createMatches(team1, hour, team2) {
+    return `
+        <li>
+            <img src="./assets/icon-${team1}.svg" alt="Bandeira do ${team1}">
+            <strong>${hour}</strong>
+            <img src="./assets/icon-${team2}.svg" alt="Bandeira da ${team2}">
+        </li>
+    `
+}
+
+function createCard(date, day, matches) {
+    return `
+    <div class="card">
+        <h2>${date} <span>${day}</span></h2>
+        <ul>
+            ${matches}
+        </ul>
+    </div>
+    `
+}
+
+document.querySelector("#app").innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW Copa">
+    </header>
+
+    <main id="cards">
+        ${createCard("24/11", "QUINTA", 
+        createMatches('brazil', '16:00', 'serbia'))}
+        ${createCard("28/11", "SEGUNDA", 
+        createMatches())}
+        ${createCard("02/12", "SEXTA", createMatches())}
+    </main>
+`
