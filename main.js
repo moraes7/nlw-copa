b1cc4093220c38c848986d076c1d9c1fe0ea7459
Nicolas Moraes <62454946+moraes7@users.noreply.github.com>
2022-11-03 14:41:22 -0300

    add matches02

--- main.js
+++ main.js
@@ -22,6 +22,6 @@ function createCard(date, day, matches) {
 }
 
 document.querySelector("#cards").innerHTML = 
-        createCard("24/11", "QUINTA", createMatches("switzerland", "07:00", "cameroon") + createMatches("portugal", "13:00", "ghana") + createMatches("brazil", "16:00", "serbia")) +
+        createCard("24/11", "QUINTA", createMatches("uruguay", "10:00", "south-korea") + createMatches("portugal", "13:00", "ghana") + createMatches("brazil", "16:00", "serbia")) +
         createCard("28/11", "SEGUNDA", createMatches("south-korea", "10:00", "ghana") +
         createMatches("brazil", "13:00", "switzerland") + createMatches("portugal", "16:00", "uruguay"))
